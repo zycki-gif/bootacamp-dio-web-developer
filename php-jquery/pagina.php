8ef860e940e4867027bd886c1cd4b33946232cf0
Juliano Xavier <julianotutti03@gmail.com>
2022-02-08 15:08:29 -0300

    first commit

---
+++ php-jquery/pagina.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+		<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Desafio-Dio Accordion</title>
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+		<link rel="stylesheet" href="/resources/demos/style.css">
+		<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+		<script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+		<script>
+		$( function() {
+			$( "#accordion" ).accordion();
+		} );
+  </script>
+	</head>
+	<body>
+		<div id="accordion">
+			<h3>Section 1</h3>
+			<div>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae massa ipsum. Donec elementum convallis turpis, porttitor bibendum eros. Donec posuere porta orci, at ornare odio semper et. Mauris a mollis ligula. Fusce et elementum tellus. Duis eget nibh at dui maximus finibus id et ante. Integer eu massa vitae quam fermentum placerat. Sed suscipit finibus magna, in gravida felis varius eget. Nam tristique ullamcorper sollicitudin. Mauris nec vehicula elit, vel cursus risus.
+				</p>
+			</div>
+			<h3>Section 2</h3>
+			<div>
+				<p>
+					Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet
+					purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor
+					velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In
+					suscipit faucibus urna.
+				</p>
+			</div>
+		</div>
+	
+	</body>
+</html>
